642897903e1ec1c568e4f22bf5ef11175e7567df
linuxct <3966313+linuxct@users.noreply.github.com>
2020-06-13 12:00:48 +0200

    reorder git commands

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-git checkout -- . ;  git pull; source .env && sudo docker build -t scrapperapp . && sudo docker-compose down && sudo docker-compose up -d
+git checkout -- . ;  git pull ; source .env && sudo docker build -t scrapperapp . && sudo docker-compose down && sudo docker-compose up -d
